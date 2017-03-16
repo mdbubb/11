f20219c30595c7cc19bde09111f3b754ef1ec472
Bubb, Matt <mdbubb@users.noreply.github.com>
2017-03-16 18:19:35 -0400

    Activity two now done

--- src/DeckTester.java
+++ src/DeckTester.java
@@ -3,24 +3,23 @@
  */
 public class DeckTester {
 
-	/**
-	 * The main method in this class checks the Deck operations for consistency.
-	 *	@param args is not used.
-	 */
-	public static void main(String[] args) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
-	String r[]= {"King", "Ace", "Jack"};;
-		String s[] = {"Heart", "Diamond","Spades"};
-		int v[] = {13,1,11};
+    /**
+     * The main method in this class checks the Deck operations for consistency.
+     *
+     * @param args is not used.
+     */
+    public static void main(String[] args) {
+        /* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
+        String r[] = {"King", "Ace", "Jack"};
+        String s[] = {"Heart", "Diamond", "Spades"};
+        int v[] = {13, 1, 11};
 
 
+        Deck deck = new Deck(r, s, v);
+        System.out.println(deck.deal());
+        System.out.println(deck.toString());
+        //Deck deck1= new Deck();
+        //Deck deck2 = new Deck();
 
-
-		Deck deck= new Deck(r,s,v);
-		System.out.println(deck.deal());
-		System.out.println(deck.toString());
-		//Deck deck1= new Deck();
-		//Deck deck2 = new Deck();
-
-	}
+    }
 }
