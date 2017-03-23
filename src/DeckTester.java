bcec6471a93329b6c93bc100a88fd48f0d1b0519
mdbubb <mdb17@thebubbs.net>
2017-03-22 21:47:27 -0400

    done activity 4

--- src/DeckTester.java
+++ src/DeckTester.java
@@ -3,47 +3,54 @@
  */
 public class DeckTester {
 
-    /**
-     * The main method in this class checks the Deck operations for consistency.
-     *
-     * @param args is not used.
-     */
-    public static void main(String[] args) {
-        /* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
+	/**
+	 * The main method in this class checks the Deck operations for consistency.
+	 *	@param args is not used.
+	 */
+	public static void main(String[] args) {
+		String[] ranks = {"jack", "queen", "king"};
+		String[] suits = {"blue", "red"};
+		int[] pointValues = {11, 12, 13};
+		Deck d = new Deck(ranks, suits, pointValues);
 
+		System.out.println("**** Original Deck Methods ****");
+		System.out.println("  toString:\n" + d.toString());
+		System.out.println("  isEmpty: " + d.isEmpty());
+		System.out.println("  size: " + d.size());
+		System.out.println();
+		System.out.println();
 
-        //first deck object
+		System.out.println("**** Deal a Card ****");
+		System.out.println("  deal: " + d.deal());
+		System.out.println();
+		System.out.println();
 
-        System.out.println("Deck object number 1:");
-        String r[] = {"King", "Ace", "Jack"};
-        String s[] = {"Heart", "Diamond", "Spades"};
-        int v[] = {13, 1, 11};
-        Deck deck = new Deck(r, s, v);
-        System.out.println(deck.deal());
-        System.out.println(deck.toString()+"\n \n \n");
+		System.out.println("**** Deck Methods After 1 Card Dealt ****");
+		System.out.println("  toString:\n" + d.toString());
+		System.out.println("  isEmpty: " + d.isEmpty());
+		System.out.println("  size: " + d.size());
+		System.out.println();
+		System.out.println();
 
-        //second deck object
-        System.out.println("Deck Object number 2:");
-        String r1[] = {"Eight", "Queen", "five"};
-        String s1[] = {"Clubs", "Diamond", "Heart"};
-        int v1[] = {8, 12, 5};
-        Deck deck1= new Deck(r1,s1,v1);
-        System.out.println(deck1.deal());
-        System.out.println(deck1.toString()+"\n \n \n");
+		System.out.println("**** Deal Remaining 5 Cards ****");
+		for (int i = 0; i < 5; i++) {
+			System.out.println("  deal: " + d.deal());
+		}
+		System.out.println();
+		System.out.println();
 
-        //third deck object
-        System.out.println("Deck Object number 3:");
-        String r2[] = {"Ace", "three", "ten"};
-        String s2[] = {"Clubs", "Spades", "Heart"};
-        int v2[] = {1, 3, 10};
-        Deck deck2= new Deck(r1,s1,v1);
-        System.out.println(deck2.deal());
-        System.out.println(deck2.toString()+"\n \n \n");
+		System.out.println("**** Deck Methods After All Cards Dealt ****");
+		System.out.println("  toString:\n" + d.toString());
+		System.out.println("  isEmpty: " + d.isEmpty());
+		System.out.println("  size: " + d.size());
+		System.out.println();
+		System.out.println();
 
+		System.out.println("**** Deal a Card From Empty Deck ****");
+		System.out.println("  deal: " + d.deal());
+		System.out.println();
+		System.out.println();
 
-
-
-        //Deck deck2 = new Deck();
-
-    }
+		/* *** TO BE COMPLETED IN ACTIVITY 4 *** */
+	}
 }
